6b23170ac53a40132cff5b16af7199ee6910a2f4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-02-10 02:48:09 +0330

    String stuff

---
+++ 03-strings.rb
@@ -0,0 +1,15 @@
+name = "Amir"
+
+puts "My name is \"Amir\""
+puts "New\nLine"
+
+puts name.upcase() # Upper case
+puts name.downcase() # Smaller case
+puts name.strip() # Remove spaces
+puts name.length() # How many charecters
+
+puts name.include? "ali" # Does it has ali or not
+
+puts name[0] # print item in a index item
+puts name[0, 2] # Print from A to m
+puts name.index("m") # Find where is m
