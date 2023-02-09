6b01676b552db5b2bd592c20792f228fb714fd61
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-02-10 02:49:36 +0330

    Number

---
+++ 04-number.rb
@@ -0,0 +1,15 @@
+current = 2023
+born = 2003
+prime = 3.14
+
+age = current - born
+
+puts "My age is " + age.to_s # Print with age to string
+
+puts -10.abs() # ABS
+puts prime.round() # Round
+puts prime.ceil() # Ceil
+puts prime.floor() # Floor
+
+puts Math.sqrt(16) # Radical
+puts Math.log(16) # Log
