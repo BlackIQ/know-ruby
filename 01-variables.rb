e1992faff6343c364cd4ae30068cd64c7e5915f0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-02-10 02:32:11 +0330

    Simple variables

---
+++ 01-variables.rb
@@ -0,0 +1,9 @@
+charecterName = "Amirhossein"
+charecterAge = "19"
+
+puts "My name is " + charecterName
+puts "I have " + charecterAge + " years ago"
+
+charecterAge = "20"
+
+puts "One year passed and now I am " + charecterAge
