bd82acf2d5033e50df1af101ee0f180c4f1f9b94
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-02-10 13:50:52 +0330

    Add for

---
+++ 12-for.rb
@@ -0,0 +1,23 @@
+friends = Array["Amirali", "Hanna", "Reza", "Hossein"]
+
+for friend in friends
+    puts friend
+end
+
+puts ""
+
+friends.each do |friend|
+    puts friend
+end
+
+puts ""
+
+for i in 0..10
+    puts i
+end
+
+puts ""
+
+10.times do |i|
+    puts i
+end
